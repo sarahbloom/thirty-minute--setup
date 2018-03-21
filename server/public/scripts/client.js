25757826c6cfa33673f923f602d60a6c041889fc
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-21 13:25:35 -0500

    controller working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,7 @@ let myApp = angular.module('myApp', []);
 
 myApp.controller('WelcomeController', function(){
     console.log("controller is working");
-    
+    let self = this;
+    self.greeting = "hi!!!";
+    self.leave = "good bye!";
 })
